65d6786ea0f07fba784683e32f89d4112d1d9eb6
TranChinh03 <21520166@gm.uit.edu.vn>
2023-09-26 08:45:38 +0700

    fix movieNavBar

--- uitheater/src/views/App.js
+++ uitheater/src/views/App.js
@@ -3,6 +3,7 @@ import About from '../views/AboutScreen/About';
 import Home from '../views/HomeScreen/Home';
 import Movies from '../views/MoviesScreen/Movies';
 import Schedule from '../views/ScheduleScreen/Schedule';
+import BookingFilter from "../components/BookingFilter/bookingFilter";
 
 import {
   BrowserRouter as Router,
