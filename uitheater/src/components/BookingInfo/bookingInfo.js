1596b9bc2388c3b2ad51d90550734574c1e57f7c
ziuwuchuoi <vv0320nt@gmail.com>
2023-10-23 10:30:49 +0700

    heh

--- uitheater/src/components/BookingInfo/bookingInfo.js
+++ uitheater/src/components/BookingInfo/bookingInfo.js
@@ -14,25 +14,25 @@ function BookingInfo() {
           </div>
           <div className='info'>
             <div className='info1'>
-              <div className='infofo' style={{ width: '230px', backgroundColor: 'white' }}>
+              <div className='infofo' style={{ width: '230px' }}>
                 <div className='info-title'>Showtime:</div>
                 <div className='info-data'>13:00</div>
               </div>
-              <div className='infofo' style={{ width: '330px', backgroundColor: 'purple' }}>
+              <div className='infofo' style={{ width: '350px' }}>
                 <div className='info-title'>Date:</div>
                 <div className='info-data'>31 - 02 - 2023</div>
               </div>
-              <div className='infofo' style={{ width: '250px', backgroundColor: 'blue' }}>
+              <div className='infofo' style={{ width: '260px' }}>
                 <div className='info-title'>Ticket:</div>
                 <div className='info-data'>2 tickets</div>
               </div>
-              <div className='infofo' style={{ width: '450px', backgroundColor: 'pink' }}>
+              <div className='infofo' style={{ width: '420px' }}>
                 <div className='info-title'>Total:</div>
                 <div className='info-data'>200.000 vnd</div>
               </div>
             </div>
             <div className='info2'>
-              <div style={{ width: '200px', backgroundColor: 'yellow' }}>Seat number:</div>
+              <div style={{ display: 'flex', flexDirection: 'column', width: '200px', backgroundColor: '#fffaf5', fontSize: '20px', color: '#0C0326', height: '100%', justifyContent: 'center', }}>Seat number:</div>
               <div></div>
             </div>
           </div>
