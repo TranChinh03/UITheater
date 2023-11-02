7e714df29a530897157b0c4cabf6d08ded56cf05
ongtrandong2 <21521957@ms.uit.edu.vn>
2023-11-02 17:46:54 +0700

    fix router

--- uitheater/src/views/App.js
+++ uitheater/src/views/App.js
@@ -1,16 +1,13 @@
-import BookingFilter from '../components/BookingFilter/bookingFilter';
-import Header from '../components/HeaderScreen/Header';
-import About from '../views/AboutScreen/About';
-import Home from '../views/HomeScreen/Home';
-import Movies from '../views/MoviesScreen/Movies';
-import Schedule from '../views/ScheduleScreen/Schedule';
-import React, {useState} from 'react';
-import {Button, Modal} from 'antd';
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
-import SignIn from '../components/SignInModal/SignIn';
-import Register from './RegisterScreen/Register';
-import NotFoundScreen from './NotFoundScreen/NotFoundScreen';
-import Info from './InfoScreen/Info';
+import Header from "../components/HeaderScreen/Header";
+import About from "../views/AboutScreen/About";
+import Home from "../views/HomeScreen/Home";
+import Movies from "../views/MoviesScreen/Movies";
+import Schedule from "../views/ScheduleScreen/Schedule";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Register from "./RegisterScreen/Register";
+import NotFoundScreen from "./NotFoundScreen/NotFoundScreen";
+import Info from "./InfoScreen/infoScreen";
 
 function App() {
   return (
