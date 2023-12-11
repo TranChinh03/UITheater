efc2e2aa16978b1068fe1984796917f7b895329a
ziuwuchuoi <vv0320nt@gmail.com>
2023-12-11 17:02:26 +0700

    chinh duoc 1 xiu

--- uitheater/src/components/moviesNavBar/moviesNavBar.js
+++ uitheater/src/components/moviesNavBar/moviesNavBar.js
@@ -8,18 +8,21 @@ function MoviesNavBar({selectedTab, onChangeTab}) {
     <div className={styles.container}>
       <ul>
         <li
+          id={'OnShow'}
           style={{zIndex: 3}}
           className={selectedTab === 'NOW SHOWING' ? styles.current : ''}
           onClick={() => onChangeTab('NOW SHOWING')}>
           <a>NOW SHOWING</a>
         </li>
         <li
+          id={'ComingShow'}
           style={{zIndex: 2}}
           className={selectedTab === 'COMING SHOW' ? styles.current : ''}
           onClick={() => onChangeTab('COMING SHOW')}>
           <a>COMING SHOW</a>
         </li>
         <li
+          id={'SpecialShow'}
           style={{zIndex: 1}}
           className={selectedTab === 'SPECIAL SCREENINGS' ? styles.current : ''}
           onClick={() => onChangeTab('SPECIAL SCREENINGS')}>
--- uitheater/src/views/MoviesScreen/Movies.js
+++ uitheater/src/views/MoviesScreen/Movies.js
@@ -1,67 +1,43 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import MovieInfo from '../../components/MovieInfo/movieInfo';
-import {Grid, Box} from '@mui/material';
+import {Grid, Box, breadcrumbsClasses} from '@mui/material';
 import styles from './moviescreen.module.scss';
 import MoviesNavBar from '../../components/moviesNavBar/moviesNavBar';
 import BookingFilter from '../../components/BookingFilter/bookingFilter';
 import {IM_Banner} from '../../assets/imgs';
-
-const movieInfoList = [
-  {
-    id: '1',
-    title: 'Shin',
-    detail: 'Noi chung la thu coi co oke kh chu biet lam gi dau huhuhuhuhuhuhu',
-    premiere: '31/12/1111',
-  },
-  {
-    id: '2',
-    title: 'Doraemon',
-    detail: 'Thiet luon la khong biet ghi cai qq gi',
-    premiere: '01/01/1111',
-  },
-  {
-    id: '3',
-    title: 'Conan',
-    detail: 'Ngoi viet may cai detail nay met moi zl',
-    premiere: '02/01/1111',
-  },
-  {
-    id: '4',
-    title: 'Hunter x Hunter',
-    detail: 'Che zl kh nghi ra duoc cai gi de test',
-    premiere: '02/02/2222',
-  },
-  {
-    id: '5',
-    title: 'Meo Di Hia',
-    detail: 'Troi dat oi ngoi nghe album cua Obito hay quaauauauauau',
-    premiere: '31/31/3131',
-  },
-  {
-    id: '6',
-    title: 'Ca Sau',
-    detail: 'Khong biet phim l gi luon',
-    premiere: '55/55/5555',
-  },
-  {
-    id: '7',
-    title: 'Huou Cao Co',
-    detail: 'Roi qua thanh the gioi dong vat luon phim phiec gi nua',
-    premiere: '88/88/8888',
-  },
-  {
-    id: '8',
-    title: 'Ca Voi Sat Thu',
-    detail: 'Mac cai gi ma bay qua ca voi luon z tr',
-    premiere: '77/77/7777',
-  },
-];
+import {getListMovieFunction} from '../../apis/GetMethod/getListMovie';
+import {BreakfastDining} from '@mui/icons-material';
 
 function Movies() {
   const [currentTab, setCurrentTab] = useState('NOW SHOWING');
+  const [movieList, setMovieList] = useState([]);
+  const [currentStatus, setCurrentStatus] = useState('OnShow');
+
   const handleTabChanged = tabName => {
     setCurrentTab(tabName);
+
+    switch (currentTab) {
+      case 'NOW SHOWING':
+        setCurrentStatus('OnShow');
+        break;
+      case 'COMING SHOW':
+        setCurrentStatus('ComingShow');
+        break;
+      case 'SPECIAL SCREENINGS':
+        setCurrentStatus('SpecialShow');
+        break;
+      default:
+        console.log('la co ra gi kh nhi?');
+        break;
+    }
   };
+
+  useEffect(() => {
+    getListMovieFunction().then(res => {
+      console.log(res);
+      setMovieList(res);
+    });
+  }, []);
 
   return (
     <>
@@ -79,23 +55,26 @@ function Movies() {
         </div>
         <div className={styles.movieListContainer}>
           <Grid container rowSpacing={1} columnSpacing={{xs: 1, sm: 2, md: 3}}>
-            {movieInfoList.map((value, id) => (
-              <Grid
-                item
-                xs={6}
-                key={id}
-                style={{
-                  display: 'flex',
-                  justifyContent: 'space-around',
-                  backgroundColor: '#231b5b',
-                  marginTop: '20px',
-                }}>
-                <MovieInfo
-                  title={value.title}
-                  detail={value.detail}
-                  premiere={value.premiere}></MovieInfo>
-              </Grid>
-            ))}
+            {movieList
+              .filter(movie => movie.status === currentStatus)
+              .map((value, status) => (
+                <Grid
+                  item
+                  xs={6}
+                  key={status}
+                  style={{
+                    display: 'flex',
+                    justifyContent: 'space-around',
+                    backgroundColor: '#231b5b',
+                    marginTop: '20px',
+                  }}>
+                  <MovieInfo
+                    src={value.image}
+                    title={value.title}
+                    detail={value.description}
+                    premiere={value.premiere}></MovieInfo>
+                </Grid>
+              ))}
           </Grid>
         </div>
       </div>
