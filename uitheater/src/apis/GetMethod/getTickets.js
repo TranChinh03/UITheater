b2134fb3eeeb589b7f8714158460f98c7a08b4ba
ongtrandong2 <21521957@ms.uit.edu.vn>
2023-12-15 13:07:37 +0700

    update api

---
+++ uitheater/src/apis/GetMethod/getTickets.js
@@ -0,0 +1,19 @@
+const axios = require('axios');
+
+
+let config = {
+  method: 'get',
+  maxBodyLength: Infinity,
+  url: 'https://uitlogachcu.onrender.com/tickets',
+  headers: { 
+    'Content-Type': 'application/json', 
+  },
+};
+
+axios.request(config)
+.then((response) => {
+  console.log(JSON.stringify(response.data));
+})
+.catch((error) => {
+  console.log(error);
+});
