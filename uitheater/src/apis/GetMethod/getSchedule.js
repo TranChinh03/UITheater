ddbff168b9ac04f9466ad3a2076d35efbf57ef07
ongtrandong2 <21521957@ms.uit.edu.vn>
2023-12-16 17:59:07 +0700

    get Schedule

---
+++ uitheater/src/apis/GetMethod/getSchedule.js
@@ -0,0 +1,20 @@
+const axios = require('axios');
+
+
+let config = {
+  method: 'get',
+  maxBodyLength: Infinity,
+  url: 'https://uitlogachcu.onrender.com/getSchedule',
+  headers: { 
+    'Content-Type': 'application/json', 
+  },
+
+};
+
+axios.request(config)
+.then((response) => {
+  console.log(JSON.stringify(response.data));
+})
+.catch((error) => {
+  console.log(error);
+});
