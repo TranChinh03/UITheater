f328e89d8631170a539de80d72e5e21660aee2e4
ongtrandong2 <21521957@ms.uit.edu.vn>
2024-01-01 03:30:42 +0700

    update api deleteprocess + change password + register + ...

---
+++ uitheater/src/apis/DeleteMethod/deleteProcess.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+export const deleteProcessFunction = async ticketId => {
+  let data = JSON.stringify({
+    ticketId: ticketId,
+  });
+
+  let config = {
+    method: 'delete',
+    maxBodyLength: Infinity,
+    url: 'https://uitlogachcu.onrender.com/getSchedule',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: data,
+  };
+
+  const result = await axios
+    .request(config)
+    .then(response => {
+      return response.data;
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  return result;
+};
---
+++ uitheater/src/apis/patchMethod/changePassword.js
@@ -0,0 +1,31 @@
+export const ChangePasswordFunction = async (email,password,newPassword) => {
+    const axios = require('axios');
+    let data = JSON.stringify({
+        email: email,
+        password: password,
+        newPassword: newPassword,
+    });
+  
+
+    let config = {
+      method: 'patch',
+      maxBodyLength: Infinity,
+      url: 'https://uitlogachcu.onrender.com/me/password',
+      headers: {
+        'Content-Type': 'application/json',
+
+      },
+      data: data,
+    };
+  
+    const result = await axios
+      .request(config)
+      .then(response => {
+        return response.data;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    return result;
+  };
+  
